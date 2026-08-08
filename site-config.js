79d9c32a43b026a928fc2e7a813e30bb70f8c5d9
adordas <104411145+adordas@users.noreply.github.com>
2026-08-08 12:17:40 -0700

    Configure CS1103 Lemon Squeezy checkout

--- site-config.js
+++ site-config.js
@@ -10,6 +10,16 @@ window.AMOOPREP_CONFIG = {
       price: "$9.99",
       checkoutUrl: "https://amooprep.lemonsqueezy.com/checkout/buy/c70c2a13-08c4-4e7c-9143-5fb74da9967c",
       pageUrl: "courses/greek-roman-civilization.html"
+    },
+    "cs1103-programming-2": {
+      title: "CS1103 Programming 2",
+      checkoutUrl: "https://amooprep.lemonsqueezy.com/checkout/buy/0fbe7fad-c20a-497e-90b6-b7d00b42b389",
+      pageUrl: "courses/cs1103-programming-2.html",
+      productId: 1277174,
+      variantId: 1996721,
+      entitlement: "cs1103-programming-2-premium",
+      licenseType: "lifetime",
+      maxActivations: 3
     }
   }
 };
